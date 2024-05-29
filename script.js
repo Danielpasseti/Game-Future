6e6721bb124a530848e2546fa0be435cf1243357
DANIEL DE FREITAS PASSETI PATRICIO <39969184881@info.local>
2024-05-29 10:57:36 -0300

    Feat: formulario feito

--- script.js
+++ script.js
@@ -8,34 +8,36 @@ function solicitarOrcamento(event) {
     let dadosForm = {
         nome: valorNome,
         email: valorEmail,
-        descricao: valorDescricao
+        descricao: valorDescricao,
     }
 
     // Enviar a requisição para a API
     // Método HTTP POST - Create/Criar -> Cadastrar um novo registro (solicitacao)
-    fetch("http://localhost:3000/solicitacoes", {
+    fetch("http://localhost:3000/formulario",{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(dadosForm)
     })
-    event.solicitarOrcamento()
-    // CASO SUCESSO
-    .then(resposta => {   
-        console.log(resposta);     
+  
+        // CASO SUCESSO
+        .then(resposta => {   
+            console.log(resposta);     
+            
+            // Limpar os inputs
+            document.querySelector("#contato form").reset()
+    
+            // Mostrar um alert de sucesso
+            alert("Solicitação enviada com sucesso!!! 👍")
+        })
+        // CASO ERRO
+        .catch(erro => {
+            console.log(erro);
+            // Mostrar alert com msg de erro
+            alert("Erro na requisição 😢")
+        })
         
-        // Limpar os inputs
-        document.querySelector("#contato form").reset()
-
-        // Mostrar um alert de sucesso
-        alert("Solicitação enviada com sucesso!!! 👍")
-    })
-    // CASO ERRO
-    .catch(erro => {
-        console.log(erro);
-        // Mostrar alert com msg de erro
-        alert("Erro na requisição 😢")
-    })
-          
-}
+        event.solicitarOrcamento()
+    }
+        
